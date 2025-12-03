f12e49595501bc10ee04e013e595f704244b170b
LOU Xun <aquarhead@gmail.com>
2025-12-03 11:42:48 +0000

    day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,7 +1,18 @@
-pub fn solve(input: &str) -> (u32, u32) {
+pub fn solve(input: &str) -> (u32, u64) {
   let p1 = input.trim().lines().map(find_largest_battery).sum();
+  let p2 = input
+    .trim()
+    .lines()
+    .map(|bank| {
+      bank
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .fold(Battery12::empty(), |bat, n| bat.expand_with(n as u8))
+        .num()
+    })
+    .sum();
 
-  (p1, 0)
+  (p1, p2)
 }
 
 fn find_largest_battery(bank: &str) -> u32 {
@@ -34,6 +45,45 @@ fn find_largest_battery(bank: &str) -> u32 {
     .2
 }
 
+#[derive(Copy, Clone, Debug)]
+struct Battery12([u8; 12]);
+
+impl Battery12 {
+  fn empty() -> Self {
+    Self([0; 12])
+  }
+
+  fn num(&self) -> u64 {
+    self.0.into_iter().fold(0u64, |acc, n| acc * 10 + (n as u64))
+  }
+
+  fn expand_with(&self, n: u8) -> Self {
+    let mut max = self.clone();
+    for i in 0..12 {
+      if self.0[i] == 0 {
+        max.0[i] = n;
+        return max;
+      }
+    }
+    for i in 0..12 {
+      let mut new_bat = self.0.clone();
+      if i > 0 {
+        new_bat.copy_within(0..i, 0);
+      }
+      if i < 11 {
+        new_bat.copy_within((i + 1)..12, i);
+      }
+      new_bat[11] = n;
+
+      let new_bat = Battery12(new_bat);
+      if new_bat.num() > max.num() {
+        max = new_bat;
+      }
+    }
+    max
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -47,6 +97,6 @@ mod tests {
 818181911112111
 ";
 
-    assert_eq!((357, 0), solve(input));
+    assert_eq!((357, 3121910778619), solve(input));
   }
 }
